e071846f92e999b8f37c08897ad7f8e78aafce32
jmnote <jmnote@users.noreply.github.com>
2017-10-23 20:48:18 +0900

    .

--- SimplePlantUML_body.php
+++ SimplePlantUML_body.php
@@ -2,27 +2,15 @@
 class SimplePlantUML {
 
 	public static function onRegistration() {
-		global $wgExtensionFunctions;
-		$wgExtensionFunctions[] = __CLASS__ . '::setup';
-		Hooks::register( 'ParserFirstCallInit', __CLASS__ . '::onParserFirstCallInit' );
+		Hooks::register( 'ParserFirstCallInit', __CLASS__ . '::setup' );
 	}
 
-	public static function setup() {
-		global $wgOut, $wgSimplePlantUMLUseCDN;
-		if( $wgSimplePlantUMLUseCDN ) {
-			$wgOut->addScript("<script src='//cdn.rawgit.com/markushedvall/plantuml-encoder/aae868f9/dist/plantuml-encoder.min.js'></script>");
-			$wgOut->addModules( 'ext.SimplePlantUML' );
-			return;
-		}
-		$wgOut->addModules(['ext.SimplePlantUML.local','ext.SimplePlantUML']);
-	}
-
-	public static function onParserFirstCallInit( Parser $parser ) {
+	public static function setup( Parser $parser ) {
 		$parser->setHook( 'uml', __CLASS__ . '::renderUML' );
-		return true;
 	}
 
-	public static function renderUML($content) {
+	public static function renderUML($content, $args, $parser) {
+		$parser->getOutput()->addModules( 'ext.SimplePlantUML' );
 		return ["<uml>$content</uml>", 'markerType'=>'nowiki'];
 	}
 
--- modules/ext.SimplePlantUML.js
+++ modules/ext.SimplePlantUML.js
@@ -1,5 +1,5 @@
-(window.RLQ=window.RLQ||[]).push(function() {
+$.getScript('//cdn.rawgit.com/markushedvall/plantuml-encoder/aae868f9/dist/plantuml-encoder.min.js').done( function() {
 	$('uml').each(function() {
-		$(this).replaceWith($('<img>').attr('src','//www.plantuml.com/plantuml/img/'+plantumlEncoder.encode($(this).text())));
+		$(this).replaceWith($('<img>').attr('src','//www.plantuml.com/plantuml/img/'+window.plantumlEncoder.encode($(this).text())));
 	})
-})
+});
