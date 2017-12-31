2741ce46632e5b9c53ab42d9569130201ebe85e6
Jmnote <banking@nate.com>
2017-12-31 22:11:49 +0900

    Update ext.SimplePlantUML.js

--- modules/ext.SimplePlantUML.js
+++ modules/ext.SimplePlantUML.js
@@ -1,4 +1,4 @@
-$.getScript('//cdn.rawgit.com/jmnote/plantuml-encoder/d133f316/dist/plantuml-encoder.min.js').done( function() {
+$.getScript('//cdn.rawgit.com/jmnote/plantuml-encoder/aee9f1be/dist/plantuml-encoder.min.js').done( function() {
 	$('uml').each(function() {
 		$(this).replaceWith($('<img>').attr('src','//www.plantuml.com/plantuml/img/'+window.plantumlEncoder.encode($(this).text())));
 	})
